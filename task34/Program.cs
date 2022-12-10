6c7250fb83a342532ae1543208b67a718b388c8c
pavel <dongod@bk.ru>
2022-12-10 17:27:38 +0400

    перенес в чистовик

---
+++ task34/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+   {
+        array[i] = new Random().Next(100, 1000);
+        Console.Write($" {array[i]} ");
+   }
+
+    return array;
+}
+
+int EventArray(int [] array)
+{
+  
+    int caunt = 0;
+    for (int x = 0; x < array.Length; x++)
+    {
+        if (array[x] % 2 == 0)
+            caunt= caunt+ 1;
+   
+    }
+    return caunt;
+}
+
+Console.WriteLine("Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+array = CreateArray(size);
+int x = EventArray(array);
+Console.WriteLine($"четных чисел {x} ");
+
---
+++ task38/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+double[] CreateArray(int size)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
+        array[i] = Math.Round(new Random().NextDouble()* (100 - (-100)) + (-100),2);
+    return array;
+}
+void PrintArray(double[] array)
+{
+    foreach (double el in array)
+        Console.Write($"{el} ");
+    Console.WriteLine();
+}
+
+
+//Console.WriteLine($"всего {array.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+
+Console.WriteLine("Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] array = CreateArray(size);
+PrintArray(array);
+double min = array[0];
+double max = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+        {
+            max = array[i];
+        }
+    if (array[i] < min)
+        {
+            min = array[i];
+        }
+}
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
